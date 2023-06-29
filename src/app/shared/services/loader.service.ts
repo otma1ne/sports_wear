64d4645e3e2692de5a24e1d28db472bc670f061d
othm1n <othmanou01@gmail.com>
2023-06-29 23:56:28 +0100

    add loader for all pages

---
+++ src/app/shared/services/loader.service.ts
@@ -0,0 +1,21 @@
+import { Injectable } from '@angular/core';
+import { BehaviorSubject } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class LoaderService {
+  public isLoading: BehaviorSubject<boolean> = new BehaviorSubject<boolean>(
+    false
+  );
+
+  show(): void {
+    this.isLoading.next(true);
+  }
+
+  hide(): void {
+    setTimeout(() => {
+      this.isLoading.next(false);
+    }, 1000); // Add a delay of 1.5 seconds before hiding the loader
+  }
+}
