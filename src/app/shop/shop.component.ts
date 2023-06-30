1eaffdfdd5a9fce2e910cffbaff21bd7e884e134
othm1n <othmanou01@gmail.com>
2023-06-30 01:11:11 +0100

    add translation module

--- src/app/shop/shop.component.ts
+++ src/app/shop/shop.component.ts
@@ -1,10 +1,20 @@
 import { Component } from '@angular/core';
+import { TranslateService } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-shop',
   templateUrl: './shop.component.html',
-  styleUrls: ['./shop.component.scss']
+  styleUrls: ['./shop.component.scss'],
 })
 export class ShopComponent {
+  constructor(private translate: TranslateService) {
+    // Set default language
+    this.translate.setDefaultLang('en');
 
+    // Add additional supported languages
+    this.translate.addLangs(['fr', 'en']);
+
+    // Use browser language as the fallback
+    this.translate.use(this.translate?.getBrowserLang() ?? 'en');
+  }
 }
