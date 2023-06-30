a082f35013aa10af87992fc4817ddbbc23d04205
othm1n <othmanou01@gmail.com>
2023-06-30 18:06:30 +0100

    fix header ( responsive / logo redirect )

--- src/app/shared/navbar/navbar.component.ts
+++ src/app/shared/navbar/navbar.component.ts
@@ -1,4 +1,5 @@
 import { Component, HostListener } from '@angular/core';
+import { Router } from '@angular/router';
 import { TranslateService } from '@ngx-translate/core';
 
 @Component({
@@ -9,14 +10,18 @@ import { TranslateService } from '@ngx-translate/core';
 export class NavbarComponent {
   isScrolled: boolean = false;
 
-  constructor(private translate: TranslateService) {
+  constructor(private translate: TranslateService, private router: Router) {
     translate.setDefaultLang('en');
   }
 
   switchLanguage(event: Event) {
     const target = event.target as HTMLSelectElement;
     const lang = target.value;
-    this.translate.use(lang); 
+    this.translate.use(lang);
+  }
+
+  navigateToHome() {
+    this.router.navigate(['/']);
   }
 
   @HostListener('window:scroll', [])
