4efcc860706b12ade443383b58b71c65665c03b8
othm1n <othmanou01@gmail.com>
2023-07-07 17:00:44 +0100

    fix config store header and newsletter

--- src/app/store/reducers/header.reducer.ts
+++ src/app/store/reducers/header.reducer.ts
@@ -8,7 +8,7 @@ import {
 } from '../actions/header.action';
 
 export const initialState = {
-  isCartOpen: true,
+  isCartOpen: false,
   isSearchOpen: false,
   isMenuOpen: false,
   isLoginOpen: false,
