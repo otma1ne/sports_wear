d9c6a8bcb1f7d54cbc08d144924f605efdd45733
othm1n <othmanou01@gmail.com>
2023-08-01 15:12:18 +0100

    change baseUrl

--- src/app/services/base-url.ts
+++ src/app/services/base-url.ts
@@ -1,2 +1,2 @@
-/* export const BASE_URL = 'https://azura-oi72.onrender.com'; */
-export const BASE_URL = 'http://localhost:5000';
+export const BASE_URL = 'https://azura-oi72.onrender.com';
+/* export const BASE_URL = 'http://localhost:5000'; */
